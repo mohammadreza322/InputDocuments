0dc3dbcad95ceb6732019aeeebfb08048210712b
amir hemmateenejad <amirhemmateenejad@googlemail.com>
2022-06-11 10:55:15 +0430

    test somthing

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,5 +1,5 @@
 export const mongoConnection: string =
-	'mongo://mongo_admin:Imfn7Tndq@185.110.189.214/chisco?authSource=admin&directConnection=true&ssl=false&readPreference=primary';
+	'mongodb://ahp:hqtYEnPAqvUm@185.110.189.214/chisco';
 
 export const jsonWebTokenSecretKey: string =
 	'CE1EF2B97E080294A562165650C850A3EBE452E08420ACEB6863E8543510FDEB';
