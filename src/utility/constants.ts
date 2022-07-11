dcaf626a0a6ec60a1900e75d367aa755f351ab08
amir hemmateenejad <amirhemmateenejad@googlemail.com>
2022-07-11 14:19:06 +0430

    fix somthing

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,5 +1,5 @@
 export const mongoConnection: string =
-	'mongodb://root:amir1234@185.110.189.214:27018/chisco?authSource=admin&readPreference=primary&directConnection=true&ssl=false';
+	'mongodb://ahpUser:acb6ApFI5797@185.204.197.144:27017/chisco?authSource=admin&readPreference=primary&directConnection=true&ssl=false';
 
 export const jsonWebTokenSecretKey: string =
 	'CE1EF2B97E080294A562165650C850A3EBE452E08420ACEB6863E8543510FDEB';
