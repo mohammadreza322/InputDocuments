5a7a9d9a0badeba367cbaf30b4342626791bdeaa
amir hemmateenejad <amirhemmateenejad@googlemail.com>
2022-10-07 16:29:35 +0330

    fix some problems

--- lib/http_client/mqtt/mqtt_classes/MqttClientFactoryServer.dart
+++ lib/http_client/mqtt/mqtt_classes/MqttClientFactoryServer.dart
@@ -2,9 +2,10 @@
 
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
+
 ///this method is for creating a connection in Mobile to  MQTT
 MqttClient makeClient(String url, String clientId) {
-  var client = MqttServerClient.withPort(url, clientId, 1883);
+  var client = MqttServerClient.withPort(url, clientId, 8888);
   // client.useWebSocket = true;
   return client;
 }
--- lib/ui/main/app_controller.dart
+++ lib/ui/main/app_controller.dart
@@ -15,6 +15,7 @@ import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../http_client/mqtt/mqtt_classes/MqttClientFactory.dart';
+
 ///AppController is Provider Controller class for Base functions or Common functions in our Project
 ///we Store all Lists and UserDetail here
 ///and if somewhere we need something we have Access to it
@@ -59,6 +60,7 @@ class AppController extends ChangeNotifier {
     convertDeviceList();
     notifyListeners();
   }
+
   ///if user didn't add any device its return false for else it's return true
   ///in the Screen if this function return false we show EmptyState for else we show list
   isUserHaveDevice() {
@@ -68,14 +70,15 @@ class AppController extends ChangeNotifier {
       return true;
     }
   }
+
   ///we have 2 type of list
   ///Cooler and Powers
   ///in this method we combine these to one
   convertDeviceList() {
-    _userDevicesList = List.from(_powers)
-      ..addAll(_coolers);
+    _userDevicesList = List.from(_powers)..addAll(_coolers);
     print("User Device List : ${_userDevicesList.toString()}");
   }
+
   ///after any editing in our app such as Add Or Edit Device , Add or Edit Schedule
   ///this method will call and its for updating or refreshing dates
   refreshData(AddDeviceResponse response) {
@@ -88,12 +91,13 @@ class AppController extends ChangeNotifier {
     print(_userDevicesList);
     notifyListeners();
   }
+
   ///after editing user detail such as Location birthday or name we refresh User Data With this method
-  refreshUserData({required String location, required String name, required num date}){
-    _user!.userDetail.address =location;
+  refreshUserData(
+      {required String location, required String name, required num date}) {
+    _user!.userDetail.address = location;
     _user!.userDetail.birthday = date;
     _user!.userDetail.fullName = name;
-
 
     notifyListeners();
   }
@@ -101,21 +105,24 @@ class AppController extends ChangeNotifier {
   ///we find Cooler in our list with serial Number and we return the Cooler
   Cooler getCoolerWithSerialNumber(String serialNumber) {
     final index =
-    _coolers.indexWhere((element) => element.serialNumber == serialNumber);
+        _coolers.indexWhere((element) => element.serialNumber == serialNumber);
     return _coolers[index];
   }
+
   ///we find Power in our list with serial Number and we return the Power
   Power getPowerWithSerialNumber(String serialNumber) {
     final index =
-    _powers.indexWhere((element) => element.serialNumber == serialNumber);
+        _powers.indexWhere((element) => element.serialNumber == serialNumber);
     return _powers[index];
   }
+
   ///we find Device in our list with serial Number and we return the Device
   Device getDeviceWithSerialNumber(String serialNumber) {
     final index = _userDevicesList
         .indexWhere((element) => element.serialNumber == serialNumber);
     return _userDevicesList[index];
   }
+
   ///we update power with this method
   ///first we find it in list and then we replace it
   setPower(Power power) {
@@ -127,49 +134,55 @@ class AppController extends ChangeNotifier {
 
     notifyListeners();
   }
+
   setCooler(Cooler selectedCooler) {
     int index = _coolers.indexWhere(
-            (element) => element.serialNumber == selectedCooler.serialNumber);
+        (element) => element.serialNumber == selectedCooler.serialNumber);
     _coolers[index] = selectedCooler;
 
     notifyListeners();
   }
+
   ///this method is for Connect to Mqtt
   connect({String? topicForSubscribe}) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    Map<String, dynamic> decodedToken = JwtDecoder.decode(sharedPreferences.getString('detail')!);
+    Map<String, dynamic> decodedToken =
+        JwtDecoder.decode(sharedPreferences.getString('detail')!);
+
     ///we need to decode Mqtt Username and password for connection
     String userNameBroker = decodedToken['usernameBroker'];
     String passwordBroker = decodedToken['passwordBroker'];
 
-    String url ='';
+    String url = '';
+
     ///if user use Web App we have to connect mqtt like this ws://[url]
     ///and if user use Mobile we don't need add anything at the start of url
-    if(kIsWeb) {
-      url = "ws://";
+    if (kIsWeb) {
+      url = "wss://";
     }
 
-    url+="chisco.tech";
-    print('UserName : ${decodedToken['usernameBroker']}\nPassword: ${decodedToken['passwordBroker']}');
+    url += "chisco.tech";
+    print(
+        'UserName : ${decodedToken['usernameBroker']}\nPassword: ${decodedToken['passwordBroker']}');
 
     MqttClient client = makeClient(url, '');
-    client.websocketProtocols = ['mqtts','mqtt'];
+    client.websocketProtocols = ['mqtts', 'mqtt'];
 
     //client.logging(on: true);
-    client.onDisconnected=(){
+    client.onDisconnected = () {
       print("disConnectttttttttt");
-
     };
 
-    client.onConnected=(){
-      //print("concteeeeeeeeeeed");
+    client.onConnected = () {
+      // print("concteeeeeeeeeeed **********************");
+      // print(client.connectionStatus!.state);
       isMqttConnected = true;
-      notifyListeners();
-
+      //notifyListeners();
     };
     final connMessage = MqttConnectMessage()
         .authenticateAs(userNameBroker, passwordBroker)
-        .withClientIdentifier('Chisco_${getUserDetail().phoneNumber}_${kIsWeb ? 'pwa' : 'mobile'}')
+        .withClientIdentifier(
+            'Chisco_${getUserDetail().phoneNumber}_${kIsWeb ? 'pwa' : 'mobile'}')
         .withWillQos(MqttQos.atMostOnce);
     client.connectionMessage = connMessage;
     try {
@@ -180,9 +193,10 @@ class AppController extends ChangeNotifier {
       if (client.connectionStatus!.state == MqttConnectionState.connected) {
         //for device
         _userDevicesList.forEach((element) {
-          client.subscribe('/chisco/${element.serialNumber}/get', MqttQos.atLeastOnce);
-         // print("Meti ::");
-          //print("subscribe '/chisco/${element.serialNumber}'");
+          client.subscribe(
+              '/chisco/${element.serialNumber}/get', MqttQos.atLeastOnce);
+          // print("Meti ::");
+          print("subscribe '/chisco/${element.serialNumber}'");
         });
         client.updates!.listen(mqttListen);
       }
@@ -193,6 +207,7 @@ class AppController extends ChangeNotifier {
 
     mqttClient = client;
   }
+
   ///for Subscribe to mqtt or Listen to Received Messages we need this method
   ///our Mqtt send serial Number of Device
   ///we get 2 thing in device
@@ -200,20 +215,25 @@ class AppController extends ChangeNotifier {
   ///if device is instance of Cooler we need to update Current state of Cooler in both view and list
 
   mqttListen(List<MqttReceivedMessage<MqttMessage?>>? c) {
-   //print('hjjhj');
-   //print(c);
+    //print('hjjhj');
+    //print(c);
     if (c != null) {
       final recMess = c[0].payload as MqttPublishMessage;
       final payloadString =
-      MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
       try {
         final payload = jsonDecode(payloadString);
         final topic = c[0].topic;
+
         ///here we find serial Number
 
-        RegExp regExp = RegExp(r'/chisco/(.*)');
+        RegExp regExp = RegExp(r'/chisco/(.*)/get');
         var matches = regExp.allMatches(topic);
         String? serialNumber = matches.first.group(1);
+        print("ok1");
+        print(serialNumber);
+        print(payload);
+
         ///update coolers
         _coolers.forEach((element) {
           if (element.serialNumber == serialNumber) {
@@ -224,20 +244,23 @@ class AppController extends ChangeNotifier {
             cooler.verticalSwing = payload['verticalSwing'];
             cooler.fan = payload['fan'];
             cooler.temp = payload['temp'];
+            cooler.power = payload['status'];
+
             setCooler(cooler);
           }
         });
+
         ///update powers
 
         _powers.forEach((element) {
           print(element.serialNumber);
           if (element.serialNumber == serialNumber) {
-
             List<Connector> connectors = [];
             for (int i = 0; i < element.connectors.length; i++) {
               Connector connector = element.connectors[i];
-              bool status = (payload['connectors'] as List<dynamic>).singleWhere((
-                  element) => element['portNumber'] == connector.connectorId)['status'];
+              bool status = (payload['connectors'] as List<dynamic>)
+                  .singleWhere((element) =>
+                      element['portNumber'] == connector.connectorId)['status'];
               connector.status = status;
               connectors.add(connector);
             }
@@ -251,8 +274,6 @@ class AppController extends ChangeNotifier {
           }
         });
 
-
-
         notifyListeners();
       } on FormatException {
         // print(e);
@@ -260,18 +281,16 @@ class AppController extends ChangeNotifier {
       }
     }
   }
+
   ///this function is for publish or send message to mqtt
   publishMessage(String topic, MqttClientPayloadBuilder data) {
     if (data.payload == null) {
       data.addString({"chisco": true}.toString());
     }
 
-
     print('published');
     mqttClient?.publishMessage(topic, MqttQos.exactlyOnce, data.payload!);
   }
-
-
 
   publishPowerMqtt(Power power) {
     List ports = [];
@@ -295,13 +314,13 @@ class AppController extends ChangeNotifier {
     coolerMap['status'] = cooler.power;
     coolerMap['temp'] = cooler.temp;
     coolerMap['mode'] = cooler.mode;
-    coolerMap['vertical_swing'] = cooler.verticalSwing;
-    coolerMap['horizontal_swing'] = cooler.horizontalSwing;
+    coolerMap['verticalSwing'] = cooler.verticalSwing;
+    coolerMap['horizontalSwing'] = cooler.horizontalSwing;
     coolerMap['fan'] = cooler.fan;
     coolerMap['timer'] = cooler.timer;
 
-
     String result = json.encode({"cooler": coolerMap});
-    publishMessage('/chisco/${cooler.serialNumber}/change', MqttClientPayloadBuilder().addString(result));
+    publishMessage('/chisco/${cooler.serialNumber}/change',
+        MqttClientPayloadBuilder().addString(result));
   }
 }
